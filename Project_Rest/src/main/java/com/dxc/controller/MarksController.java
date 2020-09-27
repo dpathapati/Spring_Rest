07dcdd6c0f0763699597f250ed5afc0e1d7aff03
dpathapati <dpathapati@host.docker.internal>
2020-09-26 17:10:51 -0700

    By Harsha

--- Project_Rest/src/main/java/com/dxc/controller/MarksController.java
+++ Project_Rest/src/main/java/com/dxc/controller/MarksController.java
@@ -3,6 +3,7 @@ package com.dxc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ public class MarksController {
 	@Autowired
 	MarksRepository marksRepository;
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@GetMapping("marks")
 	public List<Marks> getMarks()
 	{
@@ -26,6 +28,7 @@ public class MarksController {
 		return marks;
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@GetMapping(path="mark/{id}")
 	public Marks getMark(@PathVariable("id") int id) 
 	{
@@ -33,12 +36,14 @@ public class MarksController {
 		return mark;
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping("mark")
 	public Marks save(@RequestBody Marks mark)
 	{
 		return marksRepository.save(mark);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@PutMapping("mark")
 	public Marks update(Marks mark)
 	{
--- Project_Rest/src/main/java/com/dxc/controller/StudentController.java
+++ Project_Rest/src/main/java/com/dxc/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.dxc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ public class StudentController {
 	@Autowired
 	StudentRepository studentRepository;
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@GetMapping("students")
 	public List<Student> getStudents()
 	{
@@ -27,6 +29,7 @@ public class StudentController {
 		return students;
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@GetMapping(path="student/{id}")
 	public Student getStudent(@PathVariable("id") int id) 
 	{
@@ -34,18 +37,21 @@ public class StudentController {
 		return student;
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@PostMapping("student")
 	public Student save(@RequestBody Student student)
 	{
 		return studentRepository.save(student);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@PutMapping("student")
 	public Student update(Student student)
 	{
 		return ((StudentController) studentRepository).update(student);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@DeleteMapping(path = "student/{id}")
 	public void delete(@PathVariable("id") int id)
 	{
